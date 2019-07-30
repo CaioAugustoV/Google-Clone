c5153522ab3e2ad2c3c5a7ee22d622af9e225d18
CaioAugusto <caioaugustov@gmail.com>
2019-07-30 16:58:22 -0300

    Navbar + Search + Footer

---
+++ compoments/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainSearch = styled.div`
+  width: 100%;
+  height: 10px;
+  background: #000;
+`;
+
+export default function pages() {
+  return (
+    <MainSearch>
+
+    </MainSearch>
+  )
+}
